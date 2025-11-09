c415f1de0c169a78e483900b48f2082978d85352
Alex Giannakakos <alexgiannakakos@gmail.com>
2025-11-09 22:53:54 +0100

    feat(ecdh): write to file

--- ecdh.c
+++ ecdh.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -8,16 +9,16 @@ const char* TOOL_NAME = "ecdh";
 
 void usage() {
     printf ("\
-Usage: %s [OPTION]\n\
+Usage: %s [OPTION]...\n\
 ", TOOL_NAME);
     
     fputs ("\
 Perform an Elliptic-Curve Diffie-Hellman key exchange.\n\
 \n\
-  -o                       path to output file\n\
-  -a                       alice's private key\n\
-  -b                       bob's private key\n\
-  -c                       context string for key derivation\n\
+  -o [filename]            path to output file (default: prints to stdout)\n\
+  -a [integer]             alice's private key\n\
+  -b [integer]             bob's private key\n\
+  -c [string]              context string for key derivation\n\
   -h                       show this help message\n\
 ", stdout);
 
@@ -39,16 +40,17 @@ int main(int argc, char *argv[])
     }
 
     int opt;
-    char *output_path; // TODO: set default
+    char *path;
+    FILE *fd = stdout;
     uint32_t alice_pk_int, bob_pk_int; // TODO: set defaults
     unsigned char alice_pk_bytes[crypto_scalarmult_BYTES], bob_pk_bytes[crypto_scalarmult_BYTES];
-    char *context; // TODO: set defaults
+    char *context = "ECDH_KDF";
     while ((opt = getopt (argc, argv, "ho:a:b:c:")) != -1)
     {
       switch (opt)
         {
 	case 'o':
-	  output_path = optarg;
+	  path = optarg;
 	  break;
 
 	case 'a':
@@ -69,6 +71,17 @@ int main(int argc, char *argv[])
         default:
           usage ();
         }
+    }
+
+    if (path != NULL) {
+    	FILE *fd_try = fopen(path, "w");
+	if (fd_try != NULL) {
+	    fd = fd_try;
+	}
+
+	if (fd != fd_try) {
+	    fputs("Warning: Failed to process path specified by -o. Continuing with stdout...", stderr);
+	}
     }
 
     unsigned char alice_pub[crypto_scalarmult_BYTES];
@@ -98,22 +111,8 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-  /*
-    printf("Bob pk: %s\n", bob_pk_bytes);
-    printf("Bob pub: %s\n", bob_pub);
-    printf("alice pub hex addr: %p\n", alice_pub_hex);
-    printf("alice pub hex length: %lu\n", sizeof(alice_pub_hex));
-    printf("alice pub hex end addr: %p\n", alice_pub_hex+sizeof(alice_pub_hex));
-    printf("bob pub hex addr: %p\n", bob_pub_hex);
-    printf("bob pub hex length: %lu\n", sizeof(bob_pub_hex));
-    printf("bob pub hex end addr: %p\n", bob_pub_hex+sizeof(bob_pub_hex));
-    printf("Bob pub: %s\n", bob_pub);
-
-    printf("Alice priv: %s\n", alice_pub);
-    printf("Alice pub: %s\n", alice_pub_hex);
-    printf("Bob priv: %s\n", bob_pub);
-    printf("Bob pub: %s\n", bob_pub_hex);
-  */ 
+    fprintf(fd, "Alice's Public Key:\n\n%s\n\n", alice_pub_hex);
+    fprintf(fd, "Bob's Public Key:\n\n%s\n\n", bob_pub_hex);
 
     // compute shared keys
     unsigned char alice_shared_secret[crypto_scalarmult_BYTES], bob_shared_secret[crypto_scalarmult_BYTES];
@@ -139,19 +138,16 @@ int main(int argc, char *argv[])
 
     res = NULL;
     res = sodium_bin2hex(bob_shared_secret_hex, crypto_scalarmult_BYTES*64, bob_shared_secret, crypto_scalarmult_BYTES);
-//return 0;
     if (res == NULL) {
 	fputs("Error: Failed to convert Bob's shared secret from binary to hex.\n", stderr);
         exit(1);
     }
 
-  /*
-    printf("Alice shared secret: %s\n", alice_shared_secret);
-    printf("Alice shared secret: %s\n", alice_shared_secret_hex);
-    printf("Bob shared secret: %s\n", bob_shared_secret);
-    printf("Bob shared secret: %s\n", bob_shared_secret_hex);
-    printf("Are they equal? %b\n", memcmp(alice_shared_secret, bob_shared_secret, sizeof(alice_shared_secret)) == 0);
-  */
+    fprintf(fd, "Shared Secret (Alice)::\n\n%s\n\n", alice_shared_secret_hex);
+    fprintf(fd, "Shared Secret (Bob):\n\n%s\n\n", bob_shared_secret_hex);
+
+    bool is_match = memcmp(alice_shared_secret, bob_shared_secret, sizeof(alice_shared_secret)) == 0;
+    fprintf(fd, "Shared secrets %smatch!\n\n", is_match? "": "do not ");
 
     // compute symmetric key
     const int SIZE_ENCRYPTION_KEY = 32,
@@ -159,11 +155,11 @@ int main(int argc, char *argv[])
     unsigned char alice_enc_key[SIZE_ENCRYPTION_KEY], bob_enc_key[SIZE_ENCRYPTION_KEY];
     char alice_enc_key_hex[SIZE_ENCRYPTION_KEY*2+1], bob_enc_key_hex[SIZE_ENCRYPTION_KEY*2+1];
 
-    if (crypto_kdf_derive_from_key(alice_enc_key, SIZE_ENCRYPTION_KEY, 0, "ECDH_KDF", alice_shared_secret) != 0) {
+    if (crypto_kdf_derive_from_key(alice_enc_key, SIZE_ENCRYPTION_KEY, 0, context, alice_shared_secret) != 0) {
 	fputs("Error: Failed to generate Alice's encryption key.\n", stderr);
         exit(1);
     }
-    if (crypto_kdf_derive_from_key(bob_enc_key, SIZE_ENCRYPTION_KEY, 0, "ECDH_KDF", bob_shared_secret) != 0) {
+    if (crypto_kdf_derive_from_key(bob_enc_key, SIZE_ENCRYPTION_KEY, 0, context, bob_shared_secret) != 0) {
 	fputs("Error: Failed to generate Bob's encryption key.\n", stderr);
         exit(1);
     }
@@ -182,23 +178,22 @@ int main(int argc, char *argv[])
 	fputs("Error: Failed to convert Bob's shared encryption key from binary to hex.\n", stderr);
         exit(1);
     }
-/*
-    printf("Alice encryption key: %s\n", alice_enc_key);
-    printf("Alice encryption key: %s\n", alice_enc_key_hex);
-    printf("Bob encryption key: %s\n", bob_enc_key);
-    printf("Bob encryption key: %s\n", bob_enc_key_hex);
-    printf("Are they equal? %b\n", memcmp(alice_enc_key, bob_enc_key, SIZE_ENCRYPTION_KEY) == 0);
-*/
+
+    fprintf(fd, "Derived Encryption Key (Alice)::\n\n%s\n\n", alice_enc_key_hex);
+    fprintf(fd, "Derived Encryption Key (Bob):\n\n%s\n\n", bob_enc_key_hex);
+
+    is_match = memcmp(alice_enc_key, bob_enc_key, sizeof(SIZE_ENCRYPTION_KEY)) == 0;
+    fprintf(fd, "Encryption keys %smatch!\n\n", is_match? "": "do not ");
 
     // compute mac key
     unsigned char alice_mac_key[SIZE_ENCRYPTION_KEY], bob_mac_key[SIZE_ENCRYPTION_KEY];
     char alice_mac_key_hex[SIZE_ENCRYPTION_KEY*2+1], bob_mac_key_hex[SIZE_ENCRYPTION_KEY*2+1];
 
-    if (crypto_kdf_derive_from_key(alice_mac_key, SIZE_ENCRYPTION_KEY, 1, "ECDH_KDF", alice_shared_secret) != 0) {
+    if (crypto_kdf_derive_from_key(alice_mac_key, SIZE_ENCRYPTION_KEY, 1, context, alice_shared_secret) != 0) {
 	fputs("Error: Failed to generate Alice's MAC key.\n", stderr);
         exit(1);
     }
-    if (crypto_kdf_derive_from_key(bob_mac_key, SIZE_ENCRYPTION_KEY, 1, "ECDH_KDF", bob_shared_secret) != 0) {
+    if (crypto_kdf_derive_from_key(bob_mac_key, SIZE_ENCRYPTION_KEY, 1, context, bob_shared_secret) != 0) {
 	fputs("Error: Failed to generate Bob's MAC key.\n", stderr);
         exit(1);
     }
@@ -218,13 +213,11 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-  /*
-    printf("Alice MAC key: %s\n", alice_mac_key);
-    printf("Alice MAC key: %s\n", alice_mac_key_hex);
-    printf("Bob MAC key: %s\n", bob_mac_key);
-    printf("Bob MAC key: %s\n", bob_mac_key_hex);
-    printf("Are they equal? %b\n", memcmp(alice_mac_key, bob_mac_key, SIZE_MAC_KEY) == 0);
-  */
+    fprintf(fd, "Derived MAC Key (Alice)::\n\n%s\n\n", alice_mac_key_hex);
+    fprintf(fd, "Derived MAC Key (Bob):\n\n%s\n\n", bob_mac_key_hex);
+
+    is_match = memcmp(alice_mac_key, bob_mac_key, sizeof(SIZE_MAC_KEY)) == 0;
+    fprintf(fd, "MAC keys %smatch!\n\n", is_match? "": "do not ");
 
     return 0;
 }
