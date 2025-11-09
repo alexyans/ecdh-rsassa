8e401fb2dded02fe6af926122f48ad70ea78738c
Alex Giannakakos <alexgiannakakos@gmail.com>
2025-11-09 01:33:41 +0100

    init

---
+++ client.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Usage: %s <hostname> <port>\n", argv[0]);
+        exit(0);
+    }
+
+    return 0;
+}
